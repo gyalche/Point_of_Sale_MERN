36b1178ac333d250c6de49ce0fc37a9236e7b811
Dawa <sherpadawa01@outlook.com>
2022-10-19 20:35:33 +0545

    work done ready to ship

--- frontend/src/pages/BillsPage.js
+++ frontend/src/pages/BillsPage.js
@@ -1,5 +1,7 @@
 import DefaultLayout from '../components/DefaultLayout';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import ReactToPrint from 'react-to-print';
+import { useReactToPrint } from 'react-to-print';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import {
@@ -11,10 +13,11 @@ import {
 } from '@ant-design/icons';
 import { Button, Table, Modal, Form, Select, message } from 'antd';
 const BillsPage = () => {
+  const componentRef = useRef();
   const dispatch = useDispatch();
   const [billsData, setBillsData] = useState([]);
   const [popModal, setPopModal] = useState(false);
-  const [editItem, setEditItem] = useState(null);
+  const [selectedBill, setSelectedBill] = useState(null);
 
   const getAllBills = async () => {
     try {
@@ -63,7 +66,13 @@ const BillsPage = () => {
       dataIndex: '_id',
       render: (id, record) => (
         <div>
-          <EyeOutlined />
+          <EyeOutlined
+            style={{ cursor: 'pointer' }}
+            onClick={() => {
+              setSelectedBill(record);
+              setPopModal(true);
+            }}
+          />
         </div>
       ),
     },
@@ -73,6 +82,10 @@ const BillsPage = () => {
   //   getAllItems();
   // }, []);
 
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
   return (
     <DefaultLayout>
       <div className="d-flex justify-content-between">
@@ -81,13 +94,114 @@ const BillsPage = () => {
       <Table columns={columns} dataSource={billsData} bordered />;
       {popModal && (
         <Modal
-          title={`${editItem !== null ? 'Edit Item' : 'Add New Item'}`}
+          title="Invoice details"
           open={popModal}
           onCancel={() => {
-            setEditItem(null);
             setPopModal(false);
           }}
-          footer={false}></Modal>
+          footer={false}>
+          <div className="invoice-POS" ref={componentRef}>
+            <center id="top">
+              <div className="logo" />
+              <div className="info">
+                <h2>Techinfo YT POS</h2>
+                <p>Contact: 12345 | Kathmandu, gokarna</p>
+              </div>
+            </center>
+            <div className="mid">
+              <div className="mt-2">
+                <p>
+                  Customer Name: <b>{selectedBill?.customerName}</b>
+                  Phone No: <b>{selectedBill?.customerNumber}</b>
+                  <br />
+                  Date:<b>{selectedBill?.data.toString().substring(0, 10)}</b>
+                  <br />
+                  <hr style={{ margin: '5px' }} />
+                </p>
+              </div>
+            </div>
+            <div id="bot">
+              <div id="table">
+                <table>
+                  <tbody>
+                    <tr className="tabletitle">
+                      <td className="item">
+                        <h2>Item</h2>
+                      </td>
+                      <td className="Hours">
+                        <h2>Qty</h2>
+                      </td>
+                      <td className="Rate">
+                        <h2>Price</h2>
+                      </td>
+                      <td className="Rate">
+                        <h2>Total</h2>
+                      </td>
+                    </tr>
+                    {selectedBill?.cartItems.map((item) => (
+                      <>
+                        <tr className="service">
+                          <td className="tableitem">
+                            <p className="itemtext">{item?.name}</p>
+                          </td>
+                          <td className="tableitem">
+                            <p className="itemtext">{item?.quantity}</p>
+                          </td>
+                          <td className="tableitem">
+                            <p className="itemtext">{item?.price}</p>
+                          </td>
+                          <td className="tableitem">
+                            <p className="itemtext">
+                              {item?.quantity * item?.price}
+                            </p>
+                          </td>
+                        </tr>
+                      </>
+                    ))}
+
+                    <tr className="tabletitle">
+                      <td />
+                      <td />
+                      <td className="Rate">
+                        <h2>tax</h2>
+                      </td>
+                      <td className="payment">
+                        <h2>${selectedBill.tax}</h2>
+                      </td>
+                    </tr>
+                    <tr className="tabletitle">
+                      <td />
+                      <td />
+                      <td className="Rate">
+                        <h2>Grand Total</h2>
+                      </td>
+                      <td className="payment">
+                        <h2>
+                          <b>${selectedBill.totalAmount}</b>
+                        </h2>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div id="legalcopy">
+                <p className="legal">
+                  <strong>Thank you for your order!</strong> 10% GST application
+                  on total amount.Please note that this is non refundable amount
+                  for any assistance please write email
+                  <b> help@mydomain.com</b>
+                </p>
+              </div>
+            </div>
+            {/*End InvoiceBot*/}
+          </div>
+          {/*End Invoice*/}
+          <div className="d-flex justify-content-end mt-3">
+            <Button type="primary" onClick={handlePrint}>
+              Print
+            </Button>
+          </div>
+        </Modal>
       )}
     </DefaultLayout>
   );
